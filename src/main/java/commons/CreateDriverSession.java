f042c746b35d200732a454490365fe981b477e19
hieutnh <hieutnh209@gmail.com>
2021-02-22 22:04:10 +0700

    update code

--- src/main/java/commons/CreateDriverSession.java
+++ src/main/java/commons/CreateDriverSession.java
@@ -21,24 +21,28 @@ public class CreateDriverSession {
 
 		switch (platformName) {
 		case "Android":
-			caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 3");
+//			caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 3");
+			caps.setCapability(MobileCapabilityType.DEVICE_NAME, "redmi");
 			caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
 
 			// adb devices to get UDID
-			caps.setCapability(MobileCapabilityType.UDID, "emulator-5554");
+//			caps.setCapability(MobileCapabilityType.UDID, "emulator-5554");
+			caps.setCapability(MobileCapabilityType.UDID, "1876ca52");
 
 			// 1.Open android studio
 			// 2.Name of device
-			caps.setCapability("avd", "Pixel_3");
+//			caps.setCapability("avd", "Pixel_3");
 			// 3.Set time
-			caps.setCapability("avdLaunchTimeout", 180000);
+//			caps.setCapability("avdLaunchTimeout", 180000);
 
 			String apkFile = System.getProperty("user.dir") + File.separator + "APK" + File.separator + "ApiDemos-debug.apk";
+			
+			// install file apk
+			caps.setCapability(MobileCapabilityType.APP, apkFile);
+			
 			// Open app installed
 			caps.setCapability("appPackage", "io.appium.android.apis");
 			caps.setCapability("appActivity", "io.appium.android.apis.ApiDemos");
-			// install file apk
-			// caps.setCapability(MobileCapabilityType.APP, apkFile);
 
 			// unlock device
 			// caps.setCapability("unlockType", "password");
--- src/test/java/Android/Topic_01_AndroidFindElements.java
+++ src/test/java/Android/Topic_01_AndroidFindElements.java
@@ -1,4 +1,5 @@
 package Android;
+
 import org.openqa.selenium.By;
 
 import commons.CreateDriverSession;
@@ -8,30 +9,27 @@ import io.appium.java_client.MobileElement;
 
 public class Topic_01_AndroidFindElements {
 
-    public static void main(String[] args) throws Exception {
-        AppiumDriver driver = CreateDriverSession.initializeDriver("Android");
-        
-        driver.findElement(MobileBy.AccessibilityId(""));
-        driver.findElement(By.xpath(""));
+	public static void main(String[] args) throws Exception {
+		AppiumDriver driver = CreateDriverSession.initializeDriver("Android");
 
-        MobileElement myElement = (MobileElement) driver.findElementByAccessibilityId("Accessibility");
-        System.out.println(myElement.getText());
+		MobileElement myElement = (MobileElement) driver.findElementByAccessibilityId("Accessibility");
+		System.out.println(myElement.getText());
 
-        myElement = (MobileElement) driver.findElementsById("android:id/text1").get(1);
-        System.out.println(myElement.getText());
+		myElement = (MobileElement) driver.findElementsById("android:id/text1").get(1);
+		System.out.println(myElement.getText());
 
-        myElement = (MobileElement) driver.findElementsByClassName("android.widget.TextView").get(2);
-        System.out.println(myElement.getText());
+		myElement = (MobileElement) driver.findElementsByClassName("android.widget.TextView").get(2);
+		System.out.println(myElement.getText());
 
-        myElement = (MobileElement) driver.findElementByXPath("//android.widget.TextView[@content-desc=\"Accessibility\"]");
-        System.out.println(myElement.getText());
+		myElement = (MobileElement) driver.findElementByXPath("//android.widget.TextView[@content-desc=\"Accessibility\"]");
+		System.out.println(myElement.getText());
 
-        myElement = (MobileElement) driver.findElementByXPath("//*[@text=\"Accessibility\"]");
-        System.out.println(myElement.getText());
+		myElement = (MobileElement) driver.findElementByXPath("//*[@text=\"Accessibility\"]");
+		System.out.println(myElement.getText());
 
-        //not support android
+		// not support android
 //        myElement = (MobileElement) driver.findElementByTagName("Accessibility");
 //        System.out.println(myElement.getText());
 
-    }
+	}
 }
